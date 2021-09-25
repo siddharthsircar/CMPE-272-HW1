abaadfa705bae39e3a15e742124110b25a74bfa0
Siddharth Sircar <sidsircar16@gmail.com>
2021-09-24 23:04:35 -0700

    Submission HW1

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+<!DOCTYPE html>
+<html>
+<body>
+<div>
+	<?php
+		echo "Hi! This is Siddharth Sircar";
+	?>	
+</div>
+<div>
+	<?php
+		echo "Submission for CMPE-272 Home Work 1";
+	?>
+</div>
+</body>
+</html>
